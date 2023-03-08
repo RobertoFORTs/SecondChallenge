6b02909eabe7041c942265edfe27f42a32107baf
spoyis <leandrooliveira14051999@gmail.com>
2023-03-08 10:39:51 -0400

    feat: implement mongoose connection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,25 @@
 import { app } from './app';
+import { connect } from 'mongoose';
+import dotenv  from 'dotenv';
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE?.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD || ''
+);
+  
+if(DB)
+{
+  connect(DB)
+  .then(() => console.log('DB connection successful!'));
+} else{
+  console.log(`couldn't fetch connection string from DB, exiting...`);
+  process.exit(1);
+}
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Application running on port ${port}`);
+  console.log(`Application running on port ${port}`);
 });
