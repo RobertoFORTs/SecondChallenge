55110855e7b0ab07e998d74f0897fd49a3050044
RobertoFORTs <bobfortesneto@gmail.com>
2023-03-08 10:38:56 -0300

    feat(userRoutes): set up of basic routing for users

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import {router as userRouter} from "./routes/userRoutes";
 
 const app = express();
 
 app.use(express);
+
+app.use('api/v1', userRouter);
 
 export { app }
---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post('/users/signUp');
+
+router.post('/users/signIn');
+
+export {router};
