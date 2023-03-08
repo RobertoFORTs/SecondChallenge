a198339c1cb5090e71aaa0e7537410e5fc2dded5
Dener-Fernandes <dener.fernandes.oliveira@gmail.com>
2023-03-08 10:20:16 -0400

    feat: implement class AppError

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
-import express from "express";
-import {router as userRouter} from "./routes/userRoutes";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import { AppError } from "./errors/AppError";
+import { router as userRouter } from "./routes/userRoutes";
 
 const app = express();
 
 app.use(express);
 
 app.use('api/v1', userRouter);
+
+// Error handling
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({ message: error.message });  
+  }
+
+  return res.status(500).json({ message: "Internal server error" });
+});
 
 export { app }
