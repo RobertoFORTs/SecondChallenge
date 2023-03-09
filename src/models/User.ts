989b1ae3b685ddce7fdc7b12e5a9ff8a08b97804
Dener-Fernandes <dener.fernandes.oliveira@gmail.com>
2023-03-08 21:49:56 -0400

    refactor: change userSchema

--- src/models/User.ts
+++ src/models/User.ts
@@ -13,51 +13,27 @@ interface IUser {
 
 const userSchema = new Schema<IUser>({
 	firstName: {
-	type: String,
-	required: [true, "A user needs a first name."]
-},
-lastName: {
-	type: String,
-	required: [true, "A user needs a last name."]
-},
-birthDate: {
-	type: Date,
-	required: [true, "A user needs a birth Date."]
-},
-city: {
-	type: String,
-	required: [true, "Invalid city name."]
-},
-
-country: {
-	type: String,
-	required: [true, "Invalid country name."]
-},
-email: {
-	type: String,
-	required: [true, "Field email is required."],
-	lowercase: true,
-	unique: true
-},
-password: {
-	type: String,
-	required: [true, "Please provide a password."],
-	select: false //it wont show the password on the database (still needs encryption though)
-	//add a minLength later
-
-},
-confirmPassword: {
-	type: String,
-	required: [true, "Confirming password is required"],
-	validate: {
-		validator: function(this: IUser, element: String): Boolean {
-		const password: String = this.password;
-			return element === password;
-		},
-		message: "Passwords dont match!"
-		}
+		type: String,
+	},
+	lastName: {
+		type: String,
+	},
+	birthDate: {
+		type: Date,
+	},
+	city: {
+		type: String,
+	},
+	country: {
+		type: String,
+	},
+	email: {
+		type: String,
+	},
+	password: {
+		type: String,
 	}
-})
+});
 
 const User = model<IUser>("User", userSchema);
 
